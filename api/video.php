86f621c38c32e9f72028a02031eaecdb37f009c3
saiy2k <saiy2k@gmail.com>
2014-04-15 23:51:09 +0530

    Initial Commit

---
+++ api/video.php
@@ -0,0 +1,77 @@
+<?php
+/*
+TRAFFIC VIOLATIONS PORTAL
+saiy2k (http://saiy2k.blogspot.in)
+
+This file is part of Traffic Violation Portal project (https://github.com/GethuGames/Traffic-Violations-Portal).
+
+Traffic-Violations-Portal is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Traffic-Violations-Portal is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ACR-Timeline-Infograph. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+require_once('db.php');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    insertVideo($host, $username, $password, $db_name);
+} else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    returnVideos($host, $username, $password, $db_name);
+}
+
+
+function insertVideo($host, $username, $password, $db_name) {
+
+    //echo("insert Video");
+
+    $db             =   mysql_connect($host, $username, $password) or die('Could not connect');
+    mysql_select_db($db_name, $db) or die('');
+
+    $json           =   file_get_contents('php://input');
+    $obj            =   json_decode($json);
+    //print_r($obj);
+
+    $sql            =   "INSERT INTO video (videoURL, thumbURL, uploadedBy, analyzedBy, locality, town, city, pincode, time) VALUES ( '" . $obj->videoURL . "','" . $obj->thumbURL . "','" . $obj->uploadedBy . "','" . $obj->analyzedBy . "','" . $obj->locality . "','" . $obj->town . "','" . $obj->city . "'," . $obj->pincode . ",'" . $obj->time . "')";
+    //echo ($sql);
+
+    $db_insert      =   mysql_query($sql);
+
+    if (!$db_insert) {
+        die('Could not connect - event insert failed: ' . mysql_error());
+    } else {
+        echo("Success");
+    }
+
+    mysql_close($db);
+
+}
+
+function returnVideos($host, $username, $password, $db_name) {
+
+    $db             =   mysql_connect($host, $username, $password) or die('Could not connect');
+    mysql_select_db($db_name, $db) or die('could not select db');
+
+    $result         =   mysql_query("SELECT * from video") or die('Could not query');
+    $json           =   array();
+
+    if(mysql_num_rows($result)){
+        while($row=mysql_fetch_array($result, MYSQL_ASSOC)){
+            $json[] =   $row;
+        }
+    }
+
+    echo json_encode($json);
+
+    mysql_close($db);
+
+}
+
+?>
---
+++ trafficListController.js
@@ -0,0 +1,64 @@
+
+function TrafficListController($scope, $http, $log) {
+
+    // sample video data
+    $scope.videos = [{
+        videoId: 125461,
+        videoURL: 'http://www.youtube.com/dfiJKLs23j',
+        thumbURL: 'http://gfoi.com/traffic/saiy2k/yy-mm-dd-hh-mm.jpg',
+        uploadedBy: 'saiy2k',
+        analyzedBy: 'saiy3k',
+        locality: 'Kalaignar Arch',
+        town: 'Saidapet',
+        city: 'Chennai',
+        pincode: 600015,
+        time: 'yyyy-mm-dd-hh-mm',
+        officerInCharge: 'Mr. Sathappan',
+        complaints: [{ 
+        }]
+    }];
+
+    $http({ method: 'GET', url: 'http://localhost/gfoi/traffic/api/video.php' }).
+        success(function (data, status, headers, config) {
+            //$log.log("success");
+            //$log.log(data);
+            $scope.videos = data;
+        }).
+        error(function (data, status, headers, config) {
+            $log.log("error");
+            $log.log(data);
+            $scope.videos = [];
+        });
+
+    $scope.addNewVideo = function() {
+        var vid = {};
+        vid["videoURL"] = $scope.newVid.videoURL;
+        vid["thumbURL"] = $scope.newVid.videoURL;
+        vid["uploadedBy"] = "someone";
+        vid["analyzedBy"] = "";
+        vid["locality"] = $scope.newVid.locality;
+        vid["town"] = $scope.newVid.town;
+        vid["city"] = $scope.newVid.city;
+        vid["pincode"] = $scope.newVid.pincode;
+        vid["time"] = $scope.newVid.time;
+
+        $log.log(vid);
+        $log.log(JSON.stringify(vid));
+
+        var json = JSON.stringify(vid);
+
+        $http({
+            url: 'http://localhost/gfoi/traffic/api/video.php',
+            method: "POST",
+            data: json,
+            headers: {'Content-Type': 'application/json'}
+        }).success(function (data, status, headers, config) {
+            $log.log("success");
+            $log.log(data);
+        }).error(function (data, status, headers, config) {
+            $log.log("error");
+            $log.log(data);
+        });
+    };
+}
+
